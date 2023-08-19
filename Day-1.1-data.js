0ce7246aeeb938473422dc93a964c4562d167560
Nikhil Dubey <nikhildubey714@gmail.com>
2023-08-19 17:16:08 +0530

    first commit

---
+++ Day-1.1-data.js
@@ -0,0 +1,11 @@
+const data =[
+    {
+        id : 1,
+        name: "Bloody Dady",
+        img : src="https://assets.gadgets360cdn.com/pricee/assets/product/202304/Bloody-Daddy_1681628241.jpg",
+    },
+    
+
+    
+]
+console.log(data)
---
+++ Day-1.2-OrderProduction.js
@@ -0,0 +1,105 @@
+const placeorder = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log("Order is Taken")
+        callback();
+    }, 2000);
+};
+const statProduction = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log("Order is placed")
+        callback();
+    }, 1000);
+};
+const printLinux = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log(" Linux")
+        callback();
+    }, 2000);
+};
+const printGitHub = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log(" GitHub")
+        callback();
+    }, 2000);
+};
+const printHTML= (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log("HTML")
+        callback();
+    }, 2000);
+};
+const printCSS = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log("pass to CSS")
+        callback();
+    }, 2000);
+};
+const printJavaScript = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log("JavaScript")
+        callback();
+    }, 2000);
+};
+const printBootStrap = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log(" Bootstarp")
+        callback();
+    }, 2000);
+};
+const printReact = (callback)=>
+{
+    setTimeout(()=>
+    {
+        console.log("React is started")
+        callback();
+    }, 2000);
+};
+
+
+
+
+
+console.log("order is now going to take");
+placeorder(()=>{
+    console.log('pass to production' );
+    statProduction(()=>{
+        console.log('pass to Linux')
+        printLinux(()=>{
+            console.log('pass to github')
+            printGitHub(()=>{
+                console.log('pass to HTML')
+                printHTML(()=>{
+                    console.log("pass to CSS")
+                    printCSS(()=>{
+                        console.log('pass to JavaScript')
+                        printJavaScript(()=>{
+                            console.log('pass to Bootsrtap')
+                            printBootStrap(()=>{
+                                console.log('pass to React')
+                                printReact(()=>{
+                                    console.log('paasing is ended and completed')
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
